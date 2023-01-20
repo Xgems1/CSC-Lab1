8a9957bd427d5dcf56640c8a0ff39d80fd173357
Xgems1 <123202434+Xgems1@users.noreply.github.com>
2023-01-20 15:51:45 -0500

    Create firstprog.cpp

---
+++ firstprog.cpp
@@ -0,0 +1,17 @@
+// Place your name here
+#include <iostream> // needed to perform C++ I/O
+using namespace std;
+int main ()
+{
+cout << "Now is the time for all good men" << endl;
+cout << "To come to the aid of their party" << endl;
+return 0;
+}
+
+
+Output:
+
+ sh -c make -s
+ ./main
+Now is the time for all good men
+To come to the aid of their party
