c50a7e9704ea1b557a352e9893eda342bb2827ae
Xgems1 <123202434+Xgems1@users.noreply.github.com>
2023-01-20 15:08:18 -0500

    Create Lab-1-output.cpp

---
+++ Lab-1-output.cpp
@@ -0,0 +1,13 @@
+ 
+What kind of output did you get? Were you able to see the values of x and y during the program execution? 
+  sh -c make -s
+ ./main
+Orange county community college
+lab1
+content of X right after initialization: 20
+X + Y = 25
+content of y right before program exit: 5
+ 
+ 
+ Yes It was
+ 
